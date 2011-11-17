49ec4bc6b2e747373ead2943c3efa6cc469938b4
Jose Donizetti <jdbjunior@gmail.com>
2011-11-17 01:33:43 -0200

    removing validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,10 +4,5 @@ class Task < ActiveRecord::Base
   validates_associated :user
   validates_presence_of :user_id 
 
-  private
-  def validate_4_tasks_at_most
-    tasks = Task.where ["started = ? and finished_date is null",true]
-    errors.add("4_tasks", "you should not be working on more than 4 tasks") if tasks.size == 4
-  end
 
 end
